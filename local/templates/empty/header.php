eadc1ea9149360d4179d453dc3208c66fe96d624
Avutzhan Dautov <dautov92@list.ru>
2021-03-29 11:19:06 +0600

    включил автокеширование меню

--- local/templates/empty/header.php
+++ local/templates/empty/header.php
@@ -101,24 +101,24 @@ $bIsMainPage = $APPLICATION->GetCurPage(false)  == SITE_DIR;
         </header>
 
         <?$APPLICATION->IncludeComponent(
-            "bitrix:menu",
-            "menu",
-            array(
-                "COMPONENT_TEMPLATE" => "menu",
-                "ROOT_MENU_TYPE" => "top",
-                "MENU_CACHE_TYPE" => "N",
-                "MENU_CACHE_TIME" => "3600",
-                "MENU_CACHE_USE_GROUPS" => "Y",
-                "MENU_CACHE_GET_VARS" => array(
-                ),
-                "MAX_LEVEL" => "1",
-                "CHILD_MENU_TYPE" => "left",
-                "USE_EXT" => "N",
-                "DELAY" => "N",
-                "ALLOW_MULTI_SELECT" => "N"
-            ),
-            false
-        );?>
+	"bitrix:menu", 
+	"menu", 
+	array(
+		"COMPONENT_TEMPLATE" => "menu",
+		"ROOT_MENU_TYPE" => "top",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MAX_LEVEL" => "1",
+		"CHILD_MENU_TYPE" => "left",
+		"USE_EXT" => "N",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N"
+	),
+	false
+);?>
 
         <?if($bIsMainPage):?>
             <?$APPLICATION->IncludeComponent("bitrix:news.list", "index", Array(
